eb0911a5e36ff5a4913e43e9e6a4dcb01161fbac
OSXM <oscar999@mail.ustc.edu.cn>
2024-06-23 22:05:18 +0800

    FIX for JUnit

--- src/main/java/com/osxm/je/topic/security/PBEDemo.java
+++ src/main/java/com/osxm/je/topic/security/PBEDemo.java
@@ -9,8 +9,8 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+// import sun.misc.BASE64Decoder;
+// import sun.misc.BASE64Encoder;
 
 public class PBEDemo {
 	  /**
@@ -81,39 +81,39 @@ public class PBEDemo {
      * @return
      * @throws Exception
      */
-   public static byte[] decryptBASE64(String key) throws Exception {
-       return (new BASE64Decoder()).decodeBuffer(key);
-  }
+//    public static byte[] decryptBASE64(String key) throws Exception {
+//        return (new BASE64Decoder()).decodeBuffer(key);
+//   }
 
-    /**
-     * BASE64加密
-     * @param key
-     * @return
-     * @throws Exception
-     */
-    public static String encryptBASE64(byte[] key) throws Exception {
-		return (new BASE64Encoder()).encodeBuffer(key);
-   }
+//     /**
+//      * BASE64加密
+//      * @param key
+//      * @return
+//      * @throws Exception
+//      */
+//     public static String encryptBASE64(byte[] key) throws Exception {
+// 		return (new BASE64Encoder()).encodeBuffer(key);
+//    }
 
 
 	public static void main(String[] args) throws Exception {
 		String message = "你好";
 		String password = "oscar999";
-		//1. 加密
-		byte[] salt = initSalt();
-		byte[] encryBytes = encrypt(message.getBytes(),password,salt);
-		String encryStr = encryptBASE64(encryBytes);
-		String saltStr = encryptBASE64(salt);
-		System.out.println("原文="+message);
-		System.out.println("盐="+saltStr);
-		System.out.println("密文="+encryStr);
+		// //1. 加密
+		// byte[] salt = initSalt();
+		// byte[] encryBytes = encrypt(message.getBytes(),password,salt);
+		// String encryStr = encryptBASE64(encryBytes);
+		// String saltStr = encryptBASE64(salt);
+		// System.out.println("原文="+message);
+		// System.out.println("盐="+saltStr);
+		// System.out.println("密文="+encryStr);
 		
 		
-		//2. 解密
-		encryBytes = decryptBASE64(encryStr);
-		salt = decryptBASE64(saltStr);
-		byte[] decryBytes = decrypt(encryBytes,password,salt);
-		System.out.println("解密后="+new String(decryBytes));
+		// //2. 解密
+		// encryBytes = decryptBASE64(encryStr);
+		// salt = decryptBASE64(saltStr);
+		// byte[] decryBytes = decrypt(encryBytes,password,salt);
+		// System.out.println("解密后="+new String(decryBytes));
 	}
 
 }
